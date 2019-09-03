6b4da3a2fedaf8257420d11d87f98bbcab341d27
gongyanping <18380449568@163.com>
2019-09-03 18:04:31 +0800

    数据渲染

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -25,39 +25,136 @@
           <el-tab-pane label="外卖" name="takeout">外卖</el-tab-pane>
         </el-tabs>
       </el-col>
-      <el-col :span="17">我是产品栏</el-col>
+      <el-col :span="17">
+        <div class="often-goods">
+          <div class="title">常用商品</div>
+          <div class="often-goods-list">
+            <ul>
+              <li :key="item.id" v-for="item in oftenGoods">
+                <span>{{item.goodsName}}</span>
+                <span class="o-price">￥{{item.price}}元</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="goods-type">
+          <el-tabs>
+            <el-tab-pane label="汉堡">
+              <div class="cookList">
+                <ul>
+                  <li :key="item.goodsId" v-for="item in type0Goods">
+                    <span class="foodImg">
+                      <el-image
+                        :src="item.goodsImg"
+                        :fit="'cover'"
+                        style="width:100%;height:100%;"
+                      />
+                    </span>
+                    <span class="foodName">{{item.goodsName}}</span>
+                    <span class="foodPrice">￥{{item.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="小食">
+              <div class="cookList">
+                <ul>
+                  <li :key="item.goodsId" v-for="item in type1Goods">
+                    <span class="foodImg">
+                      <el-image
+                        :src="item.goodsImg"
+                        :fit="'cover'"
+                        style="width:100%;height:100%;"
+                      />
+                    </span>
+                    <span class="foodName">{{item.goodsName}}</span>
+                    <span class="foodPrice">￥{{item.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="饮料">
+              <div class="cookList">
+                <ul>
+                  <li :key="item.goodsId" v-for="item in type2Goods">
+                    <span class="foodImg">
+                      <el-image
+                        :src="item.goodsImg"
+                        :fit="'cover'"
+                        style="width:100%;height:100%;"
+                      />
+                    </span>
+                    <span class="foodName">{{item.goodsName}}</span>
+                    <span class="foodPrice">￥{{item.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="套餐">
+              <div class="cookList">
+                <ul>
+                  <li :key="item.goodsId" v-for="item in type3Goods">
+                    <span class="foodImg">
+                      <el-image
+                        :src="item.goodsImg"
+                        :fit="'cover'"
+                        style="width:100%;height:100%;"
+                      />
+                    </span>
+                    <span class="foodName">{{item.goodsName}}</span>
+                    <span class="foodPrice">￥{{item.price}}元</span>
+                  </li>
+                </ul>
+              </div>
+            </el-tab-pane>
+          </el-tabs>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       activeName: "order",
-      tableData: [
-        {
-          goodsName: "可口可乐",
-          price: 8,
-          count: 1
-        },
-        {
-          goodsName: "香辣鸡腿堡",
-          price: 15,
-          count: 1
-        },
-        {
-          goodsName: "爱心薯条",
-          price: 8,
-          count: 1
-        },
-        {
-          goodsName: "甜筒",
-          price: 8,
-          count: 1
-        }
-      ]
+      tableData: [],
+      oftenGoods: [],
+      type0Goods: [],
+      type1Goods: [],
+      type2Goods: [],
+      type3Goods: []
     };
+  },
+  created() {
+    axios
+      .get(
+        "https://www.easy-mock.com/mock/5d5b6aea61bfb2319339c364/example/oftenGoods"
+      )
+      .then(res => {
+        console.log(res);
+        if (res.data.success){
+          this.oftenGoods = res.data.result;
+        }
+      })
+      .catch(err => {
+        alert("网络错误，不能访问");
+      });
+    axios
+      .get(
+        "https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/typeGoods"
+      )
+      .then(res => {
+        this.type0Goods = res.data[0];
+        this.type1Goods = res.data[1];
+        this.type2Goods = res.data[2];
+        this.type3Goods = res.data[3];
+      })
+      .catch(err => {
+        alert("网络错误，不能访问");
+      });
   },
   mounted() {
     let orderHeight = document.body.clientHeight;
@@ -73,6 +170,60 @@ export default {
   height: 100%;
   .tool-btn {
     margin-top: 20px;
+  }
+}
+.often-goods {
+  .title {
+    height: 20px;
+    border: solid 1px #d3dce6;
+    background-color: #f9fafc;
+    padding: 10px;
+    text-align: left;
+  }
+  .often-goods-list {
+    overflow: hidden;
+    ul li {
+      list-style: none;
+      float: left;
+      border: solid 1px #e5e9fa;
+      padding: 10px;
+      margin: 10px;
+      background-color: #ffffff;
+      .o-price {
+        color: #58b7ff;
+      }
+    }
+  }
+}
+.cookList {
+  li {
+    list-style: none;
+    width: 23%;
+    border: 1px solid #e5e9f2;
+    height: auot;
+    overflow: hidden;
+    background-color: #fff;
+    padding: 2px;
+    float: left;
+    margin: 2px;
+    span {
+      display: block;
+      float: left;
+    }
+    .foodImg {
+      width: 40%;
+      height: 80px;
+    }
+    .foodName {
+      font-size: 18px;
+      padding-left: 10px;
+      color: brown;
+    }
+    .foodPrice {
+      font-size: 16px;
+      padding-left: 10px;
+      padding-top: 10px;
+    }
   }
 }
 </style>
